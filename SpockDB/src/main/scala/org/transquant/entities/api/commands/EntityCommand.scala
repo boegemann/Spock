074aebd334153cfb5c54153c87c5cd7d77e13197
Ingo Boegemann <ingo.boegemann@gmail.com>
2010-05-14 08:41:34 +0100

    Implemented first proper object serialisation test case (using Google protocol buffers

--- SpockDB/src/main/scala/org/transquant/entities/api/commands/EntityCommand.scala
+++ SpockDB/src/main/scala/org/transquant/entities/api/commands/EntityCommand.scala
@@ -16,8 +16,8 @@ case class NewEntity(entityTypeURI: String, id: UUID) extends EntityCommand
 
 case class SetEntityDetail(val entityTypeURI: String, val entityId: UUID, val entityDetailURI: String, val detailId: UUID, detail: Object) extends EntityCommand
 
-case class ConnectDetailToEntity(entityId: UUID, detailId: UUID, scope: Array[byte]) extends EntityCommand
-
-case class ReplaceDetailForEntity(entityId: UUID, scopeId:UUID, newDetailId: UUID, newDetail: Array[byte]) extends EntityCommand
-
-case class ReplaceDetailConnectionForEntity(entityId: UUID, scopeId:UUID, newDetailId: UUID) extends EntityCommand
+//case class ConnectDetailToEntity(entityId: UUID, detailId: UUID, scope: Array[byte]) extends EntityCommand
+//
+//case class ReplaceDetailForEntity(entityId: UUID, scopeId:UUID, newDetailId: UUID, newDetail: Array[byte]) extends EntityCommand
+//
+//case class ReplaceDetailConnectionForEntity(entityId: UUID, scopeId:UUID, newDetailId: UUID) extends EntityCommand
